ce639ff48105320feb9c8a3739b47bd9c667a4a0
Ckitakishi <chenyuhan43@gmail.com>
2020-12-06 14:36:29 +0900

    Day 6: Custom Customs

---
+++ AdventOfCode2020.playground/Pages/Day6.xcplaygroundpage/Contents.swift
@@ -0,0 +1,53 @@
+let batchFile =
+"""
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+"""
+
+class GroupAnswer {
+    let answers: [String]
+    var answerSet: Set<Character> = []
+    
+    init(answers: [String]) {
+        self.answers = answers
+    }
+    
+    var count1: Int {
+        answers
+            .flatMap { $0 }
+            .forEach { answerSet.insert($0) }
+        return answerSet.count
+    }
+    
+    var count2: Int {
+        answerSet = Set(answers.first ?? "")
+        answers.forEach { answerSet.formIntersection(Set($0)) }
+        return answerSet.count
+    }
+}
+
+let groupAnswers = batchFile
+    .components(separatedBy: "\n\n")
+    .map {
+        GroupAnswer(answers: $0.components(separatedBy: .newlines))
+    }
+
+// Part 1
+print(groupAnswers.reduce(0) { $0 + $1.count1 })
+
+// Part 2
+print(groupAnswers.reduce(0) { $0 + $1.count2 })
+
