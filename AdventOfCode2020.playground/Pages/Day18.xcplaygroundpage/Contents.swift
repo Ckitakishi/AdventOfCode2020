8aa37a8e90e278182f635517137679361614c53f
Ckitakishi <chenyuhan43@gmail.com>
2020-12-22 23:22:07 +0900

    Day 18: Operation Order - pt.2

--- AdventOfCode2020.playground/Pages/Day18.xcplaygroundpage/Contents.swift
+++ AdventOfCode2020.playground/Pages/Day18.xcplaygroundpage/Contents.swift
@@ -3,10 +3,15 @@
 
 let expressionInput =
 """
-1 + (2 * 3) + (4 * (5 + 6))
+((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2
 """
 
 struct BatchEvaluation {
+    
+    enum Priority {
+        case orderFirst
+        case addFirst
+    }
     
     enum Operator: String {
         case add = "+"
@@ -22,11 +27,11 @@ struct BatchEvaluation {
         }
     }
 
-    func batchEvaluate(expressions: String) -> Int {
-        expressions.componentsByLine.reduceCount { evaluate(expression: $0) }
+    func batchEvaluate(expressions: String, priority: Priority) -> Int {
+        expressions.componentsByLine.reduceCount { evaluate(expression: $0, priority: priority) }
     }
     
-    func evaluate(expression: String) -> Int {
+    private func evaluate(expression: String, priority: Priority) -> Int {
         var stack: [String] = []
         var curExpression = expression
         curExpression = curExpression.replacingOccurrences(of: "(", with: "( ")
@@ -37,29 +42,66 @@ struct BatchEvaluation {
                 guard let lastOpenParenthesis = stack.lastIndex(of: "(") else { fatalError("oops!") }
                 let tempStack = Array(stack[(lastOpenParenthesis + 1)...])
                 stack.removeLast(stack.count - lastOpenParenthesis)
-                stack.append("\(simpleEvaluate(stack: tempStack))")
+                
+                if priority == .orderFirst {
+                    stack.append("\(orderFirstEvaluate(stack: tempStack))")
+                } else {
+                    stack.append("\(addFirstEvaluate(stack: tempStack))")
+                }
             default:
                 stack.append(exp)
             }
         }
         
-        return simpleEvaluate(stack: stack)
+        return priority == .orderFirst ? orderFirstEvaluate(stack: stack) : addFirstEvaluate(stack: stack)
     }
     
-    func simpleEvaluate(stack: [String]) -> Int {
-        var preNum = Int("\(stack[0])")!
+    private func orderFirstEvaluate(stack: [String]) -> Int {
+        var preNum = Int(stack[0])!
         var op: Operator?
         stack[1...].forEach { exp in
             if op == nil, let curOp = Operator(rawValue: String(exp)) {
                 op = curOp
             } else if let curOp = op {
-                preNum = curOp.use(on: preNum, and: Int("\(exp)")!)
+                preNum = curOp.use(on: preNum, and: Int(exp)!)
                 op = nil
             }
         }
         return preNum
     }
+    
+    private func addFirstEvaluate(stack: [String]) -> Int {
+        var preNum = Int(stack[0])
+        var op: Operator?
+        var multiplyOnlyStack: [Int] = []
+        
+        stack[1...].forEach { exp in
+            if op == nil, let curOp = Operator(rawValue: String(exp)) {
+                switch curOp {
+                case .add:
+                    op = curOp
+                case .multiply:
+                    multiplyOnlyStack.append(preNum!)
+                    op = nil
+                    preNum = nil
+                }
+            } else if let curOp = op {
+                preNum = curOp.use(on: preNum!, and: Int(exp)!)
+                op = nil
+            } else {
+                preNum = Int(exp)
+            }
+        }
+        multiplyOnlyStack.append(preNum!)
+        
+        return multiplyOnlyStack.reduce(1, *)
+    }
 }
 
 let batchEvaluation = BatchEvaluation()
-print(batchEvaluation.batchEvaluate(expressions: expressionInput))
+
+// Part 1
+print(batchEvaluation.batchEvaluate(expressions: expressionInput, priority: .orderFirst))
+
+// Part 2
+print(batchEvaluation.batchEvaluate(expressions: expressionInput, priority: .addFirst))
